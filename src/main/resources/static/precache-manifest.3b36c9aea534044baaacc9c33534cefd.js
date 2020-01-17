67049eae3bd413db0ac4039c10770ae05c7e1f6c
Oldrem <51035691+Oldrem@users.noreply.github.com>
2020-01-17 07:43:41 +0300

    Add files via upload

---
+++ src/main/resources/static/precache-manifest.3b36c9aea534044baaacc9c33534cefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a05bf3f38164758cc58c1534399cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee375784ca6bd95d268f",
+    "url": "/static/css/main.b9d57b2d.chunk.css"
+  },
+  {
+    "revision": "14ede4bf61b2855dbeee",
+    "url": "/static/js/2.029977cc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.029977cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee375784ca6bd95d268f",
+    "url": "/static/js/main.18f8b700.chunk.js"
+  },
+  {
+    "revision": "405bc4e682630fa938fa",
+    "url": "/static/js/runtime-main.7991f1bd.js"
+  }
+]);
